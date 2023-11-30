8c8c63119c10f2b3b86bb9e4e8f64baae1d1ae07
rchandran7 <77090910+rchandran7@users.noreply.github.com>
2023-11-30 13:08:25 -0800

    some processing

--- preprocess_data.py
+++ preprocess_data.py
@@ -2,6 +2,15 @@ import sys
 import pandas as pd
 import numpy as np
 
+
+def dummy_variable_col(df, col):
+    df_temp = pd.get_dummies(df[col])
+    df_temp = df_temp.astype(int)
+    df = pd.concat([df, df_temp], axis=1,).reindex(df.index)
+    df.drop(col, axis=1, inplace=True)
+    return df
+
+
 if len(sys.argv) == 3:
     file_input = sys.argv[1]
     file_output = sys.argv[2]
@@ -9,4 +18,12 @@ else:
     print("Error: Invalid number of arguments.", file=sys.stderr)
 
 df = pd.read_csv(file_input)
-df.drop(['id', 'color'], axis=1)
+df = df.drop(['id', 'color'], axis=1)
+df = df.dropna()
+
+
+df = dummy_variable_col(df, 'habitat')
+
+df['Gender'] = df['Gender'].replace({True: 'male', False: 'female'})
+
+df.to_csv(file_output + '_train.csv', index=False)
