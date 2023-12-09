19ef50dba2a9cda91628d76bca966e243039ccaf
rchandran7 <77090910+rchandran7@users.noreply.github.com>
2023-12-08 16:55:19 -0800

    data+lasso colab

--- lasso.py
+++ lasso.py
@@ -1,57 +1,66 @@
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Lasso  # Lasso regression model
+# Metric for evaluating model performance
 from sklearn.metrics import mean_squared_error
+# Data preprocessing tools
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+# Hyperparameter tuning using grid search
 from sklearn.model_selection import GridSearchCV
-import pandas as pd
-import matplotlib.pyplot as plt
+import pandas as pd  # Data manipulation
+import matplotlib.pyplot as plt  # Data visualization
 
+# Read training and testing datasets
 df_train = pd.read_csv('new_fish_train.csv')
 df_test = pd.read_csv('new_fish_test.csv')
 
+# Encode 'Gender' column using LabelEncoder
 le = LabelEncoder()
 df_train['Gender'] = le.fit_transform(df_train['Gender'])
 
 # Preprocess 'Gender' column for testing set
 df_test['Gender'] = le.transform(df_test['Gender'])
 
-
+# Prepare feature and target variables for training and testing
 X_train = df_train.drop(['life_span'], axis=1)
 X_test = df_test.drop(['life_span'], axis=1)
 y_train = df_train['life_span']
 y_test = df_test['life_span']
 
-
-# Feature scaling
+# Feature scaling using StandardScaler
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-
+# Initialize Lasso regression model
 lasso = Lasso()
 param_grid = {
     'alpha': [0.001, 0.01, 0.1, 1.0],
 }
+
+# Define hyperparameter grid for GridSearchCV
 grid_search = GridSearchCV(
     lasso, param_grid, scoring='neg_mean_squared_error', cv=5)
+
+# Perform Grid Search with cross-validation for hyperparameter tuning
 grid_search.fit(X_train_scaled, y_train)
-
-
+# Get the best hyperparameters from the grid search
 best_params = grid_search.best_params_
-print("Best Hyperparameters:", best_params)
-
+# Retrieve the best model from the grid search
 best_model = grid_search.best_estimator_
+# Make predictions on the test set using the best model
 predictions = best_model.predict(X_test_scaled)
+
+# Create a DataFrame to compare actual and predicted values
 results = pd.DataFrame({'Actual': y_test, 'Predicted': predictions})
 print(results)
 
+# Calculate Mean Squared Error (MSE) as a performance metric
 mse = mean_squared_error(y_test, predictions)
 print("Mean Squared Error:", mse)
 
+# Visualize the predictions using a scatter plot
 plt.figure(figsize=(10, 6))
-plt.scatter(df_test['life_span'], predictions,
-            label='LASSO Regression', alpha=0.7)
+plt.scatter(y_test, predictions, alpha=0.7)
 plt.xlabel('Actual life_span')
 plt.ylabel('Predicted life_span')
-plt.title('Scatter Plot for LASSO Regression Model Predictions on Test Set')
-plt.legend()
+plt.title('Scatter Plot for Lasso Regression Model Predictions on Test Set')
 plt.show()
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,28 +1,40 @@
-import sys
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
+import sys  # For command line file input.
+import pandas as pd  # For easy date frame manipulation
+from sklearn.model_selection import train_test_split  # To split processed data
+
+# Function to create dummy variables for a specific column in a DataFrame
+
 
 def dummy_variable_col(df, col):
+    # Create dummy variables for the specified column
     df_temp = pd.get_dummies(df[col])
     df_temp = df_temp.astype(int)
+    # Concatenate the dummy variables to the original DataFrame and reindex
     df = pd.concat([df, df_temp], axis=1,).reindex(df.index)
+    # Drop the original column as dummy variables are created
     df.drop(col, axis=1, inplace=True)
     return df
 
+
+# Check if the correct number of command-line arguments is provided
 if len(sys.argv) == 3:
+    # Assign input and output file names from command-line arguments
     file_input = sys.argv[1]
     file_output = sys.argv[2]
 else:
+    # Print an error message if the number of arguments is invalid
     print("Error: Invalid number of arguments.", file=sys.stderr)
 
+# Read the input CSV file into a Pandas DataFrame
 df = pd.read_csv(file_input)
 df = df.drop(['id', 'color'], axis=1)
-df = df.dropna()
+df = df.dropna()  # Drop any null (empty) values
 df = dummy_variable_col(df, 'habitat')
 df['Gender'] = df['Gender'].replace({True: 'male', False: 'female'})
 
+# Split the DataFrame into training and testing sets
 train_df, test_df = train_test_split(df, test_size=0.1, random_state=42)
 
+# Write the training and testing sets to CSV files
 train_df.to_csv(file_output + '_train.csv', index=False)
 test_df.to_csv(file_output + '_test.csv', index=False)
